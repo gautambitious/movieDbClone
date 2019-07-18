e33d3e2a1e425b7e11904393c82ad19230338ef7
gautambitious <mvgt.gautam@gmail.com>
2019-07-18 19:05:36 +0530

    Now Showing Retrofit Call : Complete

---
+++ app/src/main/java/com/gautam/movies/ForgotPasswordActivity.kt
@@ -0,0 +1,22 @@
+package com.gautam.movies
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_forgot_password.*
+
+class ForgotPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/gautam/movies/GetMovies.kt
@@ -0,0 +1,2 @@
+package com.gautam.movies.com.gautam.movies
+
