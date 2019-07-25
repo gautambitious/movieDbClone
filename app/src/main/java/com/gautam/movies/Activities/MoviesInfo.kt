f4f34befff450bd321de2a988d44bb5598c14c4a
gautambitious <mvgt.gautam@gmail.com>
2019-07-25 14:30:09 +0530

    MoviesClone: App FullScreen + Collapsable toolbar add

--- app/src/main/java/com/gautam/movies/Activities/MoviesInfo.kt
+++ app/src/main/java/com/gautam/movies/Activities/MoviesInfo.kt
@@ -11,7 +11,7 @@ class MoviesInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movies_info)
-
+        setActionBar(toolbar)
 
     }
 
